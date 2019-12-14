f060978ecab79f3f75bfe2482f2feb8872c55867
ox4n4 <55216280+ox4n4@users.noreply.github.com>
2019-12-14 15:25:35 -0500

    Add files via upload

--- bartender2.0.py
+++ bartender2.0.py
@@ -3,8 +3,9 @@ import requests
 
 headers = { 'Content-Type': 'application/json'}
 
-def get_recipe():
-    api_url = 'https://www.thecocktaildb.com/api/json/'
+def get_cocktail():
+    api_url_base = 'https://www.thecocktaildb.com/api/json/v2/9973553/popular.php'
+    api_token = '9973553'
     response = requests.get(api_url, headers=headers)
 
     if response.status.code == 200:
@@ -15,7 +16,9 @@ def get_recipe():
 cocktail_info = get_recipe()
 
 if cocktail_info is not None:
-    print("Found the recipe")
+    print("Found the recipe for popular drink")
+    for cocktail in cocktail_info.items():
+        print(cocktail)
 
 else:
     print('Request failure')
